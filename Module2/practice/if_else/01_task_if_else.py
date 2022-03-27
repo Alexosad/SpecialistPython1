a94d0e6d9f12b5e33299e2dda5d90fd940acec42
Alexosad <102464242+Alexosad@users.noreply.github.com>
2022-03-27 16:27:05 +0300

    Update 01_task_if_else.py

--- Module2/practice/if_else/01_task_if_else.py
+++ Module2/practice/if_else/01_task_if_else.py
@@ -4,3 +4,8 @@
 
 number = float(input())  # Считываем вещественное число
 
+number = float(input("n: "))  # Считываем вещественное число
+if number > 0:
+	print("Положительное")
+else:
+	print("Отрицательное")
